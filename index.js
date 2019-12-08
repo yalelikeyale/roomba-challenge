a285bab4ecf2dd0623c98e8e57eaf81f33508c37
yalelikeyale <newmany89@gmail.com>
2019-12-08 09:01:05 -0700

    fixed dirt validation

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const {processInput, genNextMove} = require('./lib');
 let {roomConstructor} = require('./lib');
 let pathName = './input.txt'
 
-//create output handler
+const reportResults = (Room) =>{
+	console.log(`Roomba Search & Destroy Complete. Here are the results of the hunt: \n
+	Final Position: ${Room.roombaPosition} \n
+	Dirt Spots Destroyed: ${Room.cleanUpCount} \n`)
+}
 
+//is there a better way to use a generator
+//should check if dirt positions is empty and end job 
 const startCleaning = (Room) => {
 	let possibleMoves;
-	console.log(Room.dirtPositions)
-	// for (let i of genNextMove()){
-	// 	Room.runRoombaVacuum()
-	// 	possibleMoves = Room.runRoombaSensors();
-	// 	let movementRequest = Room.movementInstructions[i]
-	// 	if(possibleMoves.includes(movementRequest)){
-	// 		Room.updateRoombaPosition(movementRequest)
-	// 	} 
-	// }
+	for (let i of genNextMove()){
+		Room.runRoombaVacuum()
+		possibleMoves = Room.runRoombaSensors();
+		let movementRequest = Room.movementInstructions[i]
+		if(possibleMoves.includes(movementRequest)){
+			Room.updateRoombaPosition(movementRequest)
+		} 
+	}
+	reportResults(Room)
 }
 
 const startJob = async () => {
